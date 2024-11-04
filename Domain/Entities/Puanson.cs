467cdfaaeec584b12f242bae7d42dfab72aa7e41
gonzobard777 <gonzo.bard@dwfe.ru>
2024-11-05 00:51:55 +0300

    .

--- Domain/Entities/Puanson.cs
+++ Domain/Entities/Puanson.cs
@@ -5,11 +5,11 @@ using Microsoft.EntityFrameworkCore;
 namespace Domain.Entities;
 
 [PrimaryKey(nameof(Id), nameof(Date))]
-public class Puanson:IEntity
+public class Puanson : IEntity
 {
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
-    
+
     public DateTime Date { get; set; }
 
     [Column(TypeName = "jsonb")] 
--- WebApi/Controllers/PuansonController.cs
+++ WebApi/Controllers/PuansonController.cs
@@ -15,10 +15,9 @@ namespace WebApi.Controllers;
 [Route("[controller]")]
 public class PuansonController : ControllerBase
 {
-    private Random Random = new Random();
-    private string ContentStr;
-    private string PrevDateStr = "";
     private string PrevSql = "";
+    private string PrevDateStr = "";
+
     private IPuansonService Service { get; }
     private MyDbContext DbContext { get; }
 
@@ -26,52 +25,20 @@ public class PuansonController : ControllerBase
     {
         Service = service;
         DbContext = dbContext;
-        ContentStr = System.IO.File.ReadAllText(@"D:\puanson.json");
     }
 
-    private Task CreatePartition(DateTime date)
-    {
-        try
-        {
-            return ExecuteRawSql($@"
-CREATE TABLE puansons_{date.ToString("yyyyMMdd")} PARTITION OF ""Puansons""
-   FOR VALUES FROM ('{date.ToString("yyyy-MM-dd")}') TO ('{date.AddDays(1).ToString("yyyy-MM-dd")}');
-");
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e);
-            return Task.CompletedTask;
-        }
-    }
-
-    private Task ExecuteRawSql(string sql)
-    {
-        using (var command = DbContext.Database.GetDbConnection().CreateCommand())
-        {
-            command.CommandText = sql;
-            command.CommandType = CommandType.Text;
-
-            DbContext.Database.OpenConnection();
-
-            using (var result = command.ExecuteReader())
-            {
-                return Task.CompletedTask;
-            }
-        }
-    }
 
     [HttpGet("generate")]
     public async Task<ActionResult<string>> Generate()
     {
-        Console.WriteLine("Generating Puansons...");
-        Console.WriteLine($@"start: {DateTime.Now}");
         var step = 1;
-        var startDate = DateTime.SpecifyKind(DateTime.Parse("2024-05-16T00:00:00"), DateTimeKind.Utc);
-        var finalDate = DateTime.SpecifyKind(DateTime.Parse("2024-10-16T00:00:00"), DateTimeKind.Utc);
+        var startDate = DateTime.SpecifyKind(DateTime.Parse("2024-04-16T00:00:00"), DateTimeKind.Utc);
+        var finalDate = DateTime.SpecifyKind(DateTime.Parse("2024-05-16T00:00:00"), DateTimeKind.Utc);
 
         var currentDate = startDate;
         await CreatePartition(currentDate);
+        Console.WriteLine("Generating Puansons...");
+        Console.WriteLine($@"start: {DateTime.Now}");
         Console.WriteLine($@"partition: {startDate.ToString("yyyyMMdd")} at {DateTime.Now}");
         while (true)
         {
@@ -89,16 +56,7 @@ CREATE TABLE puansons_{date.ToString("yyyyMMdd")} PARTITION OF ""Puansons""
                 Console.WriteLine($@"partition: {nextDate.ToString("yyyyMMdd")} at {DateTime.Now}");
             }
 
-            // 17000 пуансонов в час
-            // for (int i = 0; i < 17000; i++)
-            //     await Service.CreateAsync(
-            //         new Puanson
-            //         {
-            //             Date = currentDate,
-            //             Data = """{"hello": """ + Random.Next() + "}"
-            //         }
-            //     );
-            await InsertRawSql(currentDate);
+            await InsertGeneratedToDB(currentDate);
 
             currentDate = nextDate;
         }
@@ -107,32 +65,88 @@ CREATE TABLE puansons_{date.ToString("yyyyMMdd")} PARTITION OF ""Puansons""
         return Ok("done");
     }
 
-    private Task InsertRawSql(DateTime date)
+    [HttpGet]
+    public async Task<PagedResult<Puanson>> List(
+        [ModelBinder(BinderType = typeof(ListQueryParamsBinder), Name = "ListQueryParams")]
+        ListQueryParams queryParams
+    )
+    {
+        return await Service.List(queryParams);
+    }
+
+
+    #region Utils
+
+    private Task ExecuteRawSql(string sql)
+    {
+        using (var command = DbContext.Database.GetDbConnection().CreateCommand())
+        {
+            command.CommandText = sql;
+            command.CommandType = CommandType.Text;
+
+            DbContext.Database.OpenConnection();
+
+            using (var result = command.ExecuteReader())
+            {
+                return Task.CompletedTask;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Создать партицию по Дате.
+    /// </summary>
+    /// <param name="date"></param>
+    /// <returns></returns>
+    private Task CreatePartition(DateTime date)
+    {
+        try
+        {
+            return ExecuteRawSql($@"
+CREATE TABLE puansons_{date.ToString("yyyyMMdd")} PARTITION OF ""Puansons""
+   FOR VALUES FROM ('{date.ToString("yyyy-MM-dd")}') TO ('{date.AddDays(1).ToString("yyyy-MM-dd")}');
+");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return Task.CompletedTask;
+        }
+    }
+
+    private Task InsertGeneratedToDB(DateTime date)
     {
         var sql = "";
         var dateStr = date.ToString("yyyyMMdd HH:mm:ss.000000");
 
+        // Сгенерировать день, если еще не сгенерирован, вида:
+        //   INSERT INTO "Puansons" ("Id", "Date", "Data") VALUES 
+        //     (1, '2024-11-03 00:00:00.000000', 'ТУТ_СОДЕРЖИМОЕ_ОДНОГО_ПУАНСОНА')
+        //     (2, '2024-11-03 00:00:00.000000', 'ТУТ_СОДЕРЖИМОЕ_ОДНОГО_ПУАНСОНА')
+        //     ...
+        //     (16999, '2024-11-03 23:00:00.000000', 'ТУТ_СОДЕРЖИМОЕ_ОДНОГО_ПУАНСОНА')
+        //     (17000, '2024-11-03 23:00:00.000000', 'ТУТ_СОДЕРЖИМОЕ_ОДНОГО_ПУАНСОНА')
         if (PrevDateStr == "" || PrevSql == "")
         {
-            var postfix = $@" '{dateStr}', '{ContentStr}')";
-            // var value = $@"(DEFAULT, '{dateStr}', '{{""hello"": {Random.Next()}}}')";
             sql = """INSERT INTO "Puansons" ("Id", "Date", "Data") VALUES """;
+
+            var contentStr = System.IO.File.ReadAllText(@"D:\puanson.json");
+            var postfix = $@" '{dateStr}', '{contentStr}')";
+            // var value = $@"(DEFAULT, '{dateStr}', '{{""hello"": {Random.Next()}}}')";
+
             var len = 17_000;
-            // var len = 1;
             for (int i = 1; i <= len; i++)
             {
                 // sql += $@"({i}, '{dateStr}', '{{""hello"": {Random.Next()}}}')";
                 sql += $@"({i}, {postfix}";
                 if (i != len)
-                {
                     sql += ',';
-                }
             }
         }
+        // Если день сгененирован, тогда взять и заменить дату.
+        // Работает намнооого быстрее, чем заново генерировать день.
         else
-        {
             sql = PrevSql.Replace(PrevDateStr, dateStr);
-        }
 
         PrevDateStr = dateStr;
         PrevSql = sql;
@@ -141,7 +155,8 @@ CREATE TABLE puansons_{date.ToString("yyyyMMdd")} PARTITION OF ""Puansons""
 
     private async Task GenerateByMinutes()
     {
-        var minutesInterval = 1;
+        var random = new Random();
+        var step = 1;
         var startDate = DateTime.SpecifyKind(new DateTime(2024, 11, 3), DateTimeKind.Utc);
         var finalDate = DateTime.SpecifyKind(new DateTime(2024, 11, 5), DateTimeKind.Utc);
 
@@ -151,7 +166,7 @@ CREATE TABLE puansons_{date.ToString("yyyyMMdd")} PARTITION OF ""Puansons""
         {
             if (currentDate >= finalDate)
                 break;
-            var nextDate = currentDate.AddMinutes(minutesInterval);
+            var nextDate = currentDate.AddMinutes(step);
             if (
                 currentDate.Day != nextDate.Day &&
                 nextDate < finalDate
@@ -164,19 +179,12 @@ CREATE TABLE puansons_{date.ToString("yyyyMMdd")} PARTITION OF ""Puansons""
                 new Puanson
                 {
                     Date = currentDate,
-                    Data = """{"hello": """ + Random.Next() + "}"
+                    Data = """{"hello": """ + random.Next() + "}"
                 }
             );
             currentDate = nextDate;
         }
     }
 
-    [HttpGet]
-    public async Task<PagedResult<Puanson>> List(
-        [ModelBinder(BinderType = typeof(ListQueryParamsBinder), Name = "ListQueryParams")]
-        ListQueryParams queryParams
-    )
-    {
-        return await Service.List(queryParams);
-    }
+    #endregion Utils
 }
